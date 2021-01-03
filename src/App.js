ec680a4e1aa106a2ca4f500f5afafe68c949fc92
funhappyit <fun.happyit@gmail.com>
2021-01-03 19:26:56 +0900

    색상 선택 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import ColorBox from "./conponents/ColorBox";
 import ColorContext, { ColorProvider } from "./contexts/color";
+import SelectColors from "./conponents/SelectColors";
 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
